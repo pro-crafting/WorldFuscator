e62f166322c78091695a0e1c5ef335ab50899b5d
Postremus <postremus1996@googlemail.com>
2015-09-12 08:21:48 +0200

    wg 5.9

--- src/main/java/de/pro_crafting/worldfuscator/WorldFuscator.java
+++ src/main/java/de/pro_crafting/worldfuscator/WorldFuscator.java
@@ -1,37 +1,28 @@
 package de.pro_crafting.worldfuscator;
 
-import org.bukkit.Bukkit;
 import org.bukkit.Location;
-import org.bukkit.Material;
 import org.bukkit.World;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import com.comphenix.example.BlockDisguiser;
-import com.comphenix.example.ChunkPacketProcessor;
-import com.comphenix.protocol.PacketType;
-import com.comphenix.protocol.ProtocolLibrary;
-import com.comphenix.protocol.events.ListenerPriority;
-import com.comphenix.protocol.events.PacketAdapter;
-import com.comphenix.protocol.events.PacketContainer;
-import com.comphenix.protocol.events.PacketEvent;
-import com.sk89q.worldguard.bukkit.RegionQuery;
 import com.sk89q.worldguard.bukkit.WorldGuardPlugin;
+import com.sk89q.worldguard.protection.ApplicableRegionSet;
 import com.sk89q.worldguard.protection.flags.DefaultFlag;
 
 public class WorldFuscator extends JavaPlugin {
-	RegionQuery query;
+	WorldGuardPlugin wg;
 
 	public void onEnable() {
 
 		new BlockDisguiser(this);
-		this.query = ((WorldGuardPlugin) Bukkit.getPluginManager().getPlugin(
-				"WorldGuard")).getRegionContainer().createQuery();
+		wg = WorldGuardPlugin.getPlugin(WorldGuardPlugin.class);
 	}
 	
 	public boolean hasRights(Player player, int x, int y, int z, World world) {
 		Location loc = new Location(world, x, y, z);
-		return query.testBuild(loc, player)
-				|| !query.testState(loc, player, DefaultFlag.ENABLE_SHOP);
+		ApplicableRegionSet ars = wg.getRegionManager(world).getApplicableRegions(loc);
+		return ars.canBuild(wg.wrapPlayer(player))
+				|| ars.allows(DefaultFlag.ENABLE_SHOP);
 	}
 }

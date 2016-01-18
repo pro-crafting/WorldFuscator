944726a7429d64e2e265cfb4aa1cb24dd5bafa65
Luki110998 <info@luki110998.de>
2016-01-18 20:21:11 +0100

    closes #3

--- src/main/java/de/pro_crafting/worldfuscator/WorldFuscator.java
+++ src/main/java/de/pro_crafting/worldfuscator/WorldFuscator.java
@@ -10,33 +10,30 @@ import org.bukkit.World;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 
-import java.util.ArrayList;
-import java.util.Iterator;
 import java.util.List;
 
 public class WorldFuscator extends JavaPlugin {
 	private WorldGuardPlugin wg;
 	private List<Integer> hideIds;
-	
+
 	public void onEnable() {
 		this.saveDefaultConfig();
 		new BlockDisguiser(this);
 		wg = WorldGuardPlugin.getPlugin(WorldGuardPlugin.class);
 		this.hideIds = this.getConfig().getIntegerList("hidden");
 	}
-	
+
 	public boolean hasRights(Player player, int x, int y, int z, World world) {
 		ApplicableRegionSet ars = wg.getRegionManager(world).getApplicableRegions(new Vector(x, y, z));
 		for (ProtectedRegion rg : ars) {
-			if (rg.isMember(player.getName()) || rg.isOwner(player.getName())) {
+			if (rg.isMember(player.getName()) || rg.isOwner(player.getName()) || player.hasPermission("worldfuscator.bypass")) {
 				return true;
 			}
 		}
 		return ars.allows(DefaultFlag.ENABLE_SHOP);
 	}
-	
-	public int translateBlockID(World world, int x, int y, int z, int blockId,
-			Player player) {
+
+	public int translateBlockID(World world, int x, int y, int z, int blockId, Player player) {
 		if (hideIds.contains(blockId)) {
 			if (!this.hasRights(player, x, y, z, world)) {
 				return 121;

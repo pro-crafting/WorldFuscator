502efe9b06e6ab4582d9e27218dabe353489481d
Martin Panzer <postremus1996@googlemail.com>
2016-01-23 13:57:12 +0100

    resend chunk once the players region domain changed Currently, the chunks are resend to all players TODO: only send the chunks to the players of the changed region closes #myplayplanet/Server#7 related to myplayplanet/regionslibrary!1

--- src/main/java/de/pro_crafting/worldfuscator/WorldFuscator.java
+++ src/main/java/de/pro_crafting/worldfuscator/WorldFuscator.java
@@ -6,13 +6,19 @@ import com.sk89q.worldguard.bukkit.WorldGuardPlugin;
 import com.sk89q.worldguard.protection.ApplicableRegionSet;
 import com.sk89q.worldguard.protection.flags.DefaultFlag;
 import com.sk89q.worldguard.protection.regions.ProtectedRegion;
+import de.pro_crafting.region.Region;
+import de.pro_crafting.region.events.RegionDomainChangeEvent;
+import org.bukkit.Bukkit;
 import org.bukkit.World;
 import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.Listener;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import java.util.List;
 
-public class WorldFuscator extends JavaPlugin {
+public class WorldFuscator extends JavaPlugin implements Listener {
 	private WorldGuardPlugin wg;
 	private List<Integer> hideIds;
 
@@ -21,10 +27,12 @@ public class WorldFuscator extends JavaPlugin {
 		new BlockDisguiser(this);
 		wg = WorldGuardPlugin.getPlugin(WorldGuardPlugin.class);
 		this.hideIds = this.getConfig().getIntegerList("hidden");
+		Bukkit.getPluginManager().registerEvents(this, this);
 	}
 
 	public boolean hasRights(Player player, int x, int y, int z, World world) {
 		ApplicableRegionSet ars = wg.getRegionManager(world).getApplicableRegions(new Vector(x, y, z));
+
 		for (ProtectedRegion rg : ars) {
 			if (rg.isMember(player.getName()) || rg.isOwner(player.getName()) || player.hasPermission("worldfuscator.bypass")) {
 				return true;
@@ -40,5 +48,16 @@ public class WorldFuscator extends JavaPlugin {
 			}
 		}
 		return blockId;
+	}
+
+	@EventHandler (priority = EventPriority.MONITOR)
+	public void handleDomainChange(RegionDomainChangeEvent event) {
+		Region updatedRegion = event.getRegion();
+		World world = updatedRegion.getWorld();
+		for (int x = updatedRegion.getMin().getX(); x < updatedRegion.getMax().getX() + 16; x += 16) {
+			for (int z = updatedRegion.getMin().getZ(); z < updatedRegion.getMax().getZ() + 16; z += 16) {
+				world.refreshChunk(x/16, z/16);
+			}
+		}
 	}
 }

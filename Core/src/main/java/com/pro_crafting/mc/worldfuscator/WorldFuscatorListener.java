7e509358a81fa3e3f3ebe3161aae6eb4a417fbb2
Martin Panzer <postremus1996@googlemail.com>
2020-04-26 12:34:04 +0200

    Make buildable (#1)
    * Refactor Maven
    Remove not needed dependencies
    clean up versions
    
    * Reformat
    
    * Remove region domain change events
    These change events where fired from an internal library, which is currently not public
    
    * Add sonarqube integration
    project encoding should allwas be utf-8
    
    * Move packing to pro-crafting
    and use a consistent packaging scheme
    
    * Create seperate plugin.yml descriptors for every plugin
    Allows clear seperation of plugins and dependencies
    
    * Only depend on spigot-api.
    No need to use the more specific paper-api, if we use no features of it
    
    * Remove lombok
    Make the initial hurdle of setting up the project easier
    
    * Some more renaming / repackaging
    
    * Remove Point class
    We can just as well use bukkits Location class
    
    * Some more documentation
    And helper methods

---
+++ Core/src/main/java/com/pro_crafting/mc/worldfuscator/WorldFuscatorListener.java
@@ -0,0 +1,49 @@
+package com.pro_crafting.mc.worldfuscator;
+
+
+import org.bukkit.Material;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.inventory.EquipmentSlot;
+import org.bukkit.inventory.ItemStack;
+
+
+public class WorldFuscatorListener implements Listener {
+
+    private WorldFuscator plugin;
+
+    public WorldFuscatorListener(WorldFuscator plugin) {
+        this.plugin = plugin;
+    }
+
+    @EventHandler(priority = EventPriority.MONITOR)
+    public void handlePlayerInteract(PlayerInteractEvent event) {
+        if (event.getHand() != EquipmentSlot.HAND) {
+            return;
+        }
+
+        if (!event.hasItem()) {
+            return;
+        }
+
+        ItemStack item = event.getItem();
+
+        if (item.getType() != Material.BLAZE_ROD) {
+            return;
+        }
+
+        Player player = event.getPlayer();
+        if (!player.hasPermission("worldfuscator.debug.toggle")) {
+            return;
+        }
+
+        boolean debugEnabled = this.plugin.getConfiguration().isDebugEnabled();
+        debugEnabled = !debugEnabled;
+        this.plugin.getConfiguration().setDebugEnabled(debugEnabled);
+
+        player.sendMessage("[WorldFuscator] Debug ist nun auf " + debugEnabled);
+    }
+}
---
+++ WorldGuard-7/src/main/java/com/pro_crafting/mc/worldfuscator/worldguard7/WorldFuscatorImpl.java
@@ -0,0 +1,75 @@
+package com.pro_crafting.mc.worldfuscator.worldguard7;
+
+import com.pro_crafting.mc.worldfuscator.BlockTranslator;
+import com.pro_crafting.mc.worldfuscator.WorldFuscator;
+import com.sk89q.worldedit.bukkit.BukkitAdapter;
+import com.sk89q.worldedit.math.BlockVector3;
+import com.sk89q.worldguard.LocalPlayer;
+import com.sk89q.worldguard.WorldGuard;
+import com.sk89q.worldguard.bukkit.WorldGuardPlugin;
+import com.sk89q.worldguard.protection.ApplicableRegionSet;
+import com.sk89q.worldguard.protection.flags.Flags;
+import com.sk89q.worldguard.protection.flags.StateFlag;
+import com.sk89q.worldguard.protection.managers.RegionManager;
+import com.sk89q.worldguard.protection.regions.ProtectedRegion;
+import com.sk89q.worldguard.protection.regions.RegionContainer;
+import org.bukkit.Bukkit;
+import org.bukkit.World;
+import org.bukkit.entity.Player;
+
+import java.util.Collection;
+import java.util.UUID;
+
+public class WorldFuscatorImpl extends WorldFuscator {
+
+    public void onEnable() {
+        setTranslator(new Translator());
+        super.onEnable();
+    }
+
+    public void updateRegion(World world, String id, Collection<UUID> oldMembers, Collection<UUID> newMembers) {
+        if (getConfiguration().isDebugEnabled()) {
+            Bukkit.getLogger().info("Chunk refresh of region: " + id);
+        }
+
+        WorldGuardPlugin wgp = WorldGuardPlugin.inst();
+        WorldGuard wg = com.sk89q.worldguard.WorldGuard.getInstance();
+
+        RegionContainer container = wg.getPlatform().getRegionContainer();
+        RegionManager rm = container.get(BukkitAdapter.adapt(world));
+        ProtectedRegion region = rm.getRegion(id);
+
+        getWorldRefresher().updateArea(world,
+                BukkitAdapter.adapt(world, region.getMinimumPoint()),
+                BukkitAdapter.adapt(world, region.getMaximumPoint()),
+                oldMembers,
+                newMembers
+        );
+    }
+
+    private class Translator extends BlockTranslator {
+
+        WorldGuardPlugin wgp = WorldGuardPlugin.inst();
+        WorldGuard wg = com.sk89q.worldguard.WorldGuard.getInstance();
+
+        @Override
+        protected boolean hasRights(Player player, int x, int y, int z, World world) {
+            LocalPlayer wgPlayer = wgp.wrapPlayer(player);
+            RegionContainer container = wg.getPlatform().getRegionContainer();
+
+            // TODO: Maybe use new Spatial Queries api for performance reasons?
+
+            RegionManager manager = container.get(BukkitAdapter.adapt(world));
+            ApplicableRegionSet ars = manager.getApplicableRegions(BlockVector3.at(x, y, z));
+
+            for (ProtectedRegion rg : ars) {
+                if (rg.isMember(wgPlayer)) {
+                    return true;
+                }
+            }
+
+            // TODO: Also allow a list of visible regions to be configured
+            return ars.queryState(wgPlayer, Flags.ENDERDRAGON_BLOCK_DAMAGE) == StateFlag.State.ALLOW;
+        }
+    }
+}

b514955fb23375bdb62d5f022d761ee30e3a6091
Martin Panzer <postremus1996@googlemail.com>
2020-05-17 20:46:53 +0200

    Fuscate blockstates when sending block change

---
+++ Core/src/main/java/com/pro_crafting/mc/worldfuscator/engine/NMSReflection.java
@@ -0,0 +1,49 @@
+package com.pro_crafting.mc.worldfuscator.engine;
+
+import com.comphenix.protocol.utility.MinecraftReflection;
+import org.bukkit.Material;
+
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
+public class NMSReflection {
+    public static Method CRAFTMAGIGNUMBERS_GET_BLOCK;
+    public static Method BLOCK_GET_STATES;
+    public static Method BLOCK_GET_COMBINED_ID;
+    public static Method BLOCKSTATELIST_GET_BLOCK_DATA_LIST;
+    public static Method REGISTRYMATERIAL_GET_ITERATOR;
+
+    public static Field IREGISTRY_BLOCK;
+
+    static {
+        Class<?> craftMagicNumbers = MinecraftReflection.getCraftBukkitClass("util.CraftMagicNumbers");
+        try {
+            CRAFTMAGIGNUMBERS_GET_BLOCK = craftMagicNumbers.getDeclaredMethod("getBlock", Material.class);
+
+            Class<?> blockClass = MinecraftReflection.getBlockClass();
+            BLOCK_GET_STATES = blockClass.getDeclaredMethod("getStates");
+            BLOCK_GET_COMBINED_ID = blockClass.getMethod("getCombinedId", MinecraftReflection.getIBlockDataClass());
+
+            Class<?> blockStateList = MinecraftReflection.getMinecraftClass("BlockStateList");
+            BLOCKSTATELIST_GET_BLOCK_DATA_LIST = blockStateList.getDeclaredMethod("a");
+
+            Class<?> registryMaterials = MinecraftReflection.getMinecraftClass("RegistryMaterials");
+            REGISTRYMATERIAL_GET_ITERATOR = registryMaterials.getDeclaredMethod("iterator");
+
+            Class<?> iRegistry = MinecraftReflection.getMinecraftClass("IRegistry");
+            IREGISTRY_BLOCK = iRegistry.getDeclaredField("BLOCK");
+        } catch (NoSuchMethodException | NoSuchFieldException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Gets the combined id from an nms blockData using the block registry
+     * @param blockData
+     * @return
+     */
+    public static int getCombinedId(Object blockData) throws InvocationTargetException, IllegalAccessException {
+        return (int) NMSReflection.BLOCK_GET_COMBINED_ID.invoke(null, blockData);
+    }
+}
--- Core/src/main/java/com/pro_crafting/mc/worldfuscator/engine/palette/GlobalPaletteAdapter.java
+++ Core/src/main/java/com/pro_crafting/mc/worldfuscator/engine/palette/GlobalPaletteAdapter.java
@@ -1,14 +1,12 @@
 package com.pro_crafting.mc.worldfuscator.engine.palette;
 
-import com.comphenix.protocol.utility.MinecraftReflection;
 import com.google.common.collect.ImmutableList;
+import com.pro_crafting.mc.worldfuscator.engine.NMSReflection;
 import it.unimi.dsi.fastutil.ints.IntArrayList;
 import it.unimi.dsi.fastutil.ints.IntList;
 import org.bukkit.Material;
 
-import java.lang.reflect.Field;
 import java.lang.reflect.InvocationTargetException;
-import java.lang.reflect.Method;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Set;
@@ -19,36 +17,6 @@ import java.util.Set;
  * This class uses reflection. For ease of migration to newer minecraft versions, this class should contain as few lines as possible.
  */
 public class GlobalPaletteAdapter {
-    private Method getBlock;
-    private Method getStates;
-    private Method getBlockDataList;
-    private Method getCombinedId;
-    private Method getRegistryMaterialsIterator;
-
-    private Field iRegistryBlock;
-
-    public GlobalPaletteAdapter() {
-        Class<?> craftMagicNumbers = MinecraftReflection.getCraftBukkitClass("util.CraftMagicNumbers");
-        try {
-            getBlock = craftMagicNumbers.getDeclaredMethod("getBlock", Material.class);
-
-            Class<?> blockClass = MinecraftReflection.getBlockClass();
-            getStates = blockClass.getDeclaredMethod("getStates");
-            getCombinedId = blockClass.getMethod("getCombinedId", MinecraftReflection.getIBlockDataClass());
-
-            Class<?> blockStateList = MinecraftReflection.getMinecraftClass("BlockStateList");
-            getBlockDataList = blockStateList.getDeclaredMethod("a");
-
-            Class<?> registryMaterials = MinecraftReflection.getMinecraftClass("RegistryMaterials");
-            getRegistryMaterialsIterator = registryMaterials.getDeclaredMethod("iterator");
-
-            Class<?> iRegistry = MinecraftReflection.getMinecraftClass("IRegistry");
-            iRegistryBlock = iRegistry.getDeclaredField("BLOCK");
-        } catch (NoSuchMethodException | NoSuchFieldException e) {
-            e.printStackTrace();
-        }
-    }
-
     /**
      * Get all possible state ids of a material from the global palette. For example, a stair has a state id for every rotation it can be in.
      * This id is also used when transfering the state of a block through the network.
@@ -58,17 +26,14 @@ public class GlobalPaletteAdapter {
      */
     public IntList getAllStateIds(Material material) {
         try {
-            Object block = getBlock.invoke(null, material);
-            Object states = getStates.invoke(block);
-            Object stateList = getBlockDataList.invoke(states);
+            Object block = NMSReflection.CRAFTMAGIGNUMBERS_GET_BLOCK.invoke(null, material);
+            Object states = NMSReflection.BLOCK_GET_STATES.invoke(block);
             @SuppressWarnings("unchecked")
-            ImmutableList<Object> casted = (ImmutableList<Object>) stateList;
+            ImmutableList<Object> blockDataList =  (ImmutableList<Object>) NMSReflection.BLOCKSTATELIST_GET_BLOCK_DATA_LIST.invoke(states);
 
-            IntList globalPaletteList = new IntArrayList(casted.size());
-
-            for (Object blockData : casted) {
-                int id = (int) getCombinedId.invoke(null, blockData);
-                globalPaletteList.add(id);
+            IntList globalPaletteList = new IntArrayList(blockDataList.size());
+            for (Object blockData : blockDataList) {
+                globalPaletteList.add(NMSReflection.getCombinedId(blockData));
             }
 
             return globalPaletteList;
@@ -85,24 +50,23 @@ public class GlobalPaletteAdapter {
         }
 
         try {
-            Object o = iRegistryBlock.get(null);
+            Object o = NMSReflection.IREGISTRY_BLOCK.get(null);
             @SuppressWarnings("unchecked")
-            Iterator<Object> blockIterator = (Iterator<Object>)getRegistryMaterialsIterator.invoke(o);
+            Iterator<Object> blockIterator = (Iterator<Object>)NMSReflection.REGISTRYMATERIAL_GET_ITERATOR.invoke(o);
 
             IntList globalPaletteList = new IntArrayList();
             while (blockIterator.hasNext()) {
                 Object block = blockIterator.next();
 
-                Object states = getStates.invoke(block);
+                Object states = NMSReflection.BLOCK_GET_STATES.invoke(block);
                 @SuppressWarnings("unchecked")
-                ImmutableList<Object> blockDataList =  (ImmutableList<Object>)getBlockDataList.invoke(states);
+                ImmutableList<Object> blockDataList =  (ImmutableList<Object>)NMSReflection.BLOCKSTATELIST_GET_BLOCK_DATA_LIST.invoke(states);
 
                 for (Object blockData : blockDataList) {
                     for (Set<String> requestedStates : requestedStateList) {
                         for (String requestedState : requestedStates) {
                             if (blockData.toString().contains(requestedState)) {
-                                int id = (int) getCombinedId.invoke(null, blockData);
-                                globalPaletteList.add(id);
+                                globalPaletteList.add(NMSReflection.getCombinedId(blockData));
                             }
                         }
                     }
